b6787a886db411bcd3df10a95092ed27a340f9dc
Susmita-Dey <susmitadey475@gmail.com>
2023-05-13 22:20:11 +0530

    Add footer

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -1,41 +1,15 @@
 import React from 'react'
-import { Footer } from 'flowbite-react'
 
 export default function FooterComponent() {
     return (
-        <footer className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-16 bg-gray-900'>
-            <Footer container={true} bgDark={true}>
-                <div className="w-full text-center">
-                    <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-                        <Footer.Brand
-                            href="https://flowbite.com"
-                            src="https://flowbite.com/docs/images/logo.svg"
-                            alt="Flowbite Logo"
-                            name="Flowbite"
-                        />
-                        <Footer.LinkGroup>
-                            <Footer.Link href="#">
-                                About
-                            </Footer.Link>
-                            <Footer.Link href="#">
-                                Privacy Policy
-                            </Footer.Link>
-                            <Footer.Link href="#">
-                                Licensing
-                            </Footer.Link>
-                            <Footer.Link href="#">
-                                Contact
-                            </Footer.Link>
-                        </Footer.LinkGroup>
-                    </div>
-                    <Footer.Divider />
-                    <Footer.Copyright
-                        href="#"
-                        by="Flowbite™"
-                        year={2022}
-                    />
+        <footer className="mt-12 body-font">
+            <div className="bg-blue-950 text-white w-full">
+                <div className="container mx-auto py-6 px-24 flex flex-wrap flex-col">
+                    <p className="text-base text-center font-medium">
+                        &copy; 2023 CodeOpsEditor | Powered by <Link href='/#team'>CodeOpsEditor Team</Link>
+                    </p>
                 </div>
-            </Footer>
+            </div>
         </footer>
     )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import FaqComponent from './components/FAQComponent';
 import Features from './components/Features';
 import FooterComponent from './components/FooterComponent';
 import HeroSection from './components/HeroSection';
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Navbar />
       <HeroSection />
       <Features />
+      <FaqComponent />
       <FooterComponent />
     </>
   </React.StrictMode>,
