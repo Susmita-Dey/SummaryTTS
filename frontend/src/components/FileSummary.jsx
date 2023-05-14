e4677fd3afcd4e549e7f40e041f5a82d922eec00
Jaisal Srivastava <48391316+JaiS99@users.noreply.github.com>
2023-05-14 18:26:53 +0530

    modified:   frontend/src/components/FileSummary.jsx

--- frontend/src/components/FileSummary.jsx
+++ frontend/src/components/FileSummary.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import { useState } from 'react'
+ 
 
 export default function FileSummary() {
-
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+    fetch('/upload', {
+      method: 'POST',
+      body: formData
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Network response was not ok.');
+    }).then((data) => {
+      const textSummary = data.textSummary;
+      const brailleSummary = data.brailleSummary;
+      const readAloudSummary = data.readAloudSummary;
+  
+      // Update the content of the summary elements
+      document.getElementById('text-summary').textContent = textSummary;
+      document.getElementById('braille-summary').textContent = brailleSummary;
+      document.getElementById('read-aloud-summary').textContent = readAloudSummary;
+      
+    }).catch((error) => {
+      console.error('There was a problem with the network request:', error);
+    });
+  };
+  
   return (
     <div>
       <div className='flex flex-col justify-center items-center m-16 p-12 '>
         <p className='font-bold m-12 text-6xl text-center'>Generate Summary Of A File</p>
-        <input type="file" id="helper-text" aria-describedby="helper-text-explanation" className="w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  mt-12" placeholder="Select the file of which you want summary" />
-        <a href="#second" className='flex justify-center items-center h-16 w-64 text-2xl rounded-2xl border-solid border-2 border-black bg-white mt-16 text-black'>GENERATE</a>
+        <input onChange={handleFileChange} type="file" id="helper-text" aria-describedby="helper-text-explanation" className="w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  mt-12" placeholder="Select the file of which you want summary" />
+        <a onAuxClick={handleSubmit} className='flex justify-center items-center h-16 w-64 text-2xl rounded-2xl border-solid border-2 border-black bg-white mt-16 text-black'>GENERATE</a>
       </div>
+      <div id="text-summary"></div>
+      <div id="braille-summary"></div>
+      <div id="read-aloud-summary"></div>
 
       <div id="second" className="flex flex-col justify-center items-center m-16 mb-64 mt-64 p-12">
         <p className="font-bold m-12 text-6xl text-center">In Which Way You Want Summary ? </p>
@@ -22,3 +56,5 @@ export default function FileSummary() {
     </div>
   )
 }
+
+
