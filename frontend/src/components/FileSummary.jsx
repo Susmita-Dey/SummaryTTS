a8a5c026b967a5f4fa351949e373b02aeeb371b5
Susmita-Dey <susmitadey475@gmail.com>
2023-05-14 18:41:53 +0530

    button

--- frontend/src/components/FileSummary.jsx
+++ frontend/src/components/FileSummary.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
- 
+
 
 export default function FileSummary() {
   const [file, setFile] = useState(null);
@@ -22,23 +22,23 @@ export default function FileSummary() {
       const textSummary = data.textSummary;
       const brailleSummary = data.brailleSummary;
       const readAloudSummary = data.readAloudSummary;
-  
+
       // Update the content of the summary elements
       document.getElementById('text-summary').textContent = textSummary;
       document.getElementById('braille-summary').textContent = brailleSummary;
       document.getElementById('read-aloud-summary').textContent = readAloudSummary;
-      
+
     }).catch((error) => {
       console.error('There was a problem with the network request:', error);
     });
   };
-  
+
   return (
     <div>
       <div className='flex flex-col justify-center items-center m-16 p-12 '>
         <p className='font-bold m-12 text-6xl text-center'>Generate Summary Of A File</p>
         <input onChange={handleFileChange} type="file" id="helper-text" aria-describedby="helper-text-explanation" className="w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  mt-12" placeholder="Select the file of which you want summary" />
-        <a onAuxClick={handleSubmit} className='flex justify-center items-center h-16 w-64 text-2xl rounded-2xl border-solid border-2 border-black bg-white mt-16 text-black'>GENERATE</a>
+        <button onClick={handleSubmit} className='flex justify-center items-center h-16 w-64 text-2xl rounded-2xl border-solid border-2 border-black bg-white mt-16 text-black'>GENERATE</button>
       </div>
       <div id="text-summary"></div>
       <div id="braille-summary"></div>
